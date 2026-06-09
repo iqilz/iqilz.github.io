6619c850bf58ee87d0cb8c33a60f3bc21e78357b
iqilz <danielgkchan@gmail.com>
2026-06-09 19:56:51 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ const track = document.getElementById("media-track");
 let percentage = -10;
 
 window.onwheel = e => {
-    if (window.innerWidth < 768) return;
-
     const scrollDelta = e.deltaY / 25;
     const nextPercentage = Math.max(Math.min(percentage - scrollDelta, -10), -90);
     
@@ -25,33 +23,31 @@ window.onwheel = e => {
 
 var form = document.getElementById("my-form");
     
-if (form) {
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          status.innerHTML = "Thanks for your submission!";
-          form.reset();
-        } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
-            } else {
-              status.innerHTML = "Oops! There was a problem submitting your form";
-            }
-          })
-        }
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form";
-      });
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
     }
-    form.addEventListener("submit", handleSubmit);
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset();
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form";
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form";
+  });
 }
+form.addEventListener("submit", handleSubmit)
